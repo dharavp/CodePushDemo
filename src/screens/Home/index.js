661982ba6b9acd823e7f84fc34bcee6a0490b4ab
Dhara Patel <dhara@creolestudios.com>
2019-01-11 17:26:29 +0530

    change button text and second screen text

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -11,7 +11,7 @@ export default class Home extends Component {
                 justifyContent: 'center'
             }}>
                 <Text>
-                    Home screen
+                    Second screen
                 </Text>
             </View>
         );
