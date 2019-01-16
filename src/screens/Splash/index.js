ba1780013fe2bba8ae3e542880a7528b2a34032d
Dhara Patel <dhara@creolestudios.com>
2019-01-16 12:07:01 +0530

    changed button text

--- src/screens/Splash/index.js
+++ src/screens/Splash/index.js
@@ -22,7 +22,7 @@ export default class Splash extends Component {
                         backgroundColor: 'green',
                         borderRadius: 50
                     }}>
-                        <Text style={{ color: '#000' }}>Brijeshbhai</Text>
+                        <Text style={{ color: '#000' }}>click on me</Text>
                     </View>
                 </TouchableOpacity>
             </View>
